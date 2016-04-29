f55c4db7c3906bf1df3b4bfc3199c504cd1cc42d
Kanishka Kaushik <kaushikkanishka@gmail.com>
2016-04-29 14:45:23 +0530

    Changes made in TC001

--- features/pages/base.rb
+++ features/pages/base.rb
@@ -10,4 +10,8 @@ class Base
     urls = YAML.load_file(File.dirname(__FILE__) + '/../../lib/urls.yml')
     urls['URLS'][env]
   end
+
+  def read_time_stamp
+    @time = Time.now.utc.strftime('%Y%m%d%H%M%S')
+  end
 end
